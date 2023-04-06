79a848374de91d2794faf6c47cb38afb789cc13e
Clara <clarapachecodev@gmail.com>
2023-04-06 09:12:32 -0300

    Import component Button and use it inside of Details page

--- src/pages/Details/Details.jsx
+++ src/pages/Details/Details.jsx
@@ -1,9 +1,12 @@
 import { Container } from "./styles"
 
+import { Button } from "../../components/Button/Button"
+
 export function Details() {
   return(
     <Container >
       <h1>Clara Pacheco</h1>
+      <Button />
     </Container>
   )
    
