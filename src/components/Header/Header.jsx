c1adb9620949aa990ca4d20dafdb3f89803d6a7c
Clara <clarapachecodev@gmail.com>
2023-04-06 19:29:38 -0300

    Import Profile and add it inside the Header component with the styles for elements img, span and strong

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,25 @@
-import { Container } from './Styles'
+import { Container, Profile } from './Styles'
 
 export function Header() {
   return(
-    <Container />
+    <Container>
+       <Profile>
+          <img 
+            src="https://github.com/Clara-Pacheco.png"
+            alt='Imagem do usuário'
+          />
+
+          <div>
+              <span>Bem vindo,</span>
+              <strong>Clara Pacheco</strong>
+          </div>
+
+
+       </Profile>
+
+
+
+    </Container>
   )
 
 }
