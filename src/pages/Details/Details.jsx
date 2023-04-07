e45940d14e656579c7391bc50c87576a3fde8c6d
Clara <clarapachecodev@gmail.com>
2023-04-07 19:30:36 -0300

    Create Links css component to style ul inside of Details page component

--- src/pages/Details/Details.jsx
+++ src/pages/Details/Details.jsx
@@ -1,12 +1,24 @@
-import { Container } from "./styles"
+import { Container, Links} from "./styles"
 
 import { Header } from "../../components/Header/Header"
 import { Button } from "../../components/Button/Button"
+import { Section } from "../../components/Section/Section"
 
 export function Details() {
   return(
     <Container >
       <Header />
+      <Section title="Links úteis">
+      <Links>
+          <li>
+            <a href="https://www.rocketseat.com.br"></a>
+          </li>
+          <li>Item 2</li>
+          <li>Item 3</li>
+      </Links>
+      </Section>
+
+
       <Button title = 'Voltar'/>
       
     </Container>
--- src/pages/Details/styles.js
+++ src/pages/Details/styles.js
@@ -13,3 +13,18 @@ import styled from "styled-components";
 
 `;
 
+export const Links = styled.ul`
+  list-style: none;
+
+    > li {
+      margin-top: 12px;
+
+
+      a {
+        color: ${({ theme }) => theme.COLORS.WHITE};
+      }
+
+    }
+
+`;
+
