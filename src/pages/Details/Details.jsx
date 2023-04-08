bcff1cf87fcd3370817fae68e11f4aa272ba82d2
Clara <clarapachecodev@gmail.com>
2023-04-08 09:01:42 -0300

    Fix passing content for section 'Links úteis'

--- src/pages/Details/Details.jsx
+++ src/pages/Details/Details.jsx
@@ -10,12 +10,15 @@ export function Details() {
       <Header />
       <Section title="Links úteis">
       <Links>
-          <li>
-            <a href="https://www.rocketseat.com.br"></a>
-          </li>
-          <li>Item 2</li>
-          <li>Item 3</li>
+          <li><a href="#">https://www.rocketseat.com.br</a></li>
+          <li><a href="#">https://www.rocketseat.com.br</a></li>
+          <li><a href="#">https://www.rocketseat.com.br</a></li>   
       </Links>
+      </Section>
+
+      <Section title="Marcadores">
+        <li>express</li>
+        <li>nodejs</li>
       </Section>
 
 
