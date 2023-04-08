59d2c089cd5b22300e35dc09a499c1d1ae939603
Clara <clarapachecodev@gmail.com>
2023-04-08 10:02:47 -0300

    Create Tag folder containing Tag component and css Tag component

---
+++ src/components/Tag/Styles.js
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+
+  display: inline;
+  margin-right: 6px;
+
+  width: 64px;
+  height: 24px;
+
+  font-size: 12px;
+
+  padding: 5px 14px;
+  border-radius: 5px;
+
+  color: ${({ theme }) => theme.COLORS.BACKGROUND_900};
+  background-color: ${({ theme }) => theme.COLORS.ORANGE};
+
+
+
+
+
+`;
---
+++ src/components/Tag/Tag.jsx
@@ -0,0 +1,9 @@
+import { Container } from './Styles'
+
+export function Tag({ title }) {
+  return (
+    <Container>
+      {title}
+    </Container>
+  )
+}
