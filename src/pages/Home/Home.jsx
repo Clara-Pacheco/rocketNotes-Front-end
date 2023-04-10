c58f43e684a9f4d62a99fd12eb1ff09491ed2c37
Clara <clarapachecodev@gmail.com>
2023-04-10 16:30:43 -0300

    Import ButtonText and use it inside the Home Menu css component passing title and isActive as properties

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,7 @@
 import { Container, Brand, Search, Content, Menu, NewNote } from './Styles.js'
 
 import { Header } from '../../components/Header/Header'
+import { ButtonText } from '../../components/ButtonText/ButtonText'
 
 export function Home(){
 
@@ -13,6 +14,12 @@ export function Home(){
       <Header />
 
       <Menu>
+
+        <li><ButtonText title="Todos" isActive /></li>
+        <li><ButtonText title="Frontend" /></li>
+        <li><ButtonText title="Node" /></li>
+        <li><ButtonText title="React" /></li>
+      
 
       </Menu>
 
