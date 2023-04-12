ac6837c81431c8e85894a4da8d08d029ca9a9cc4
Clara <clarapachecodev@gmail.com>
2023-04-12 15:38:58 -0300

    Substitute <Home /> page component for <SignIn /> page component

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { Home } from './pages/Home/Home'
+// import { Home } from './pages/Home/Home'
 import { ThemeProvider } from 'styled-components'
 import GlobalStyles from './styles/global'
 import theme from './styles/theme'
+import { SignIn } from './pages/SignIn/SignIn'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-       <Home />
+       <SignIn />
     </ThemeProvider>
   </React.StrictMode>,
 )
