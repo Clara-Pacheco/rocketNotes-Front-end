8203270c9885c041cec6b5f9c42ecba144404b55
Clara <clarapachecodev@gmail.com>
2023-04-13 11:37:44 -0300

    Import Form css component and Input component and build header section of Form css component inside of New page component

--- src/pages/New/New.jsx
+++ src/pages/New/New.jsx
@@ -1,11 +1,23 @@
-import { Container } from './Styles'
+import { Container, Form } from './Styles'
 
 import { Header } from '../../components/Header/Header'
+import { Input } from '../../components/Input/Input'
 
 export function New(){
   return (
     <Container>
       <Header/>
+      <main>
+        <Form>
+          <header>
+            <h1>Criar nota</h1>
+            <a href="/">voltar</a>
+          </header>
+
+          <Input placeholder="Título"/>
+
+        </Form>
+      </main>
     </Container>
   )
 
