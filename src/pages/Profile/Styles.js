ca48cbdd256c23dacdb400565b3093331f639255
Clara <clarapachecodev@gmail.com>
2023-04-13 09:43:55 -0300

    Build Container css component and Form css component

--- src/pages/Profile/Styles.js
+++ src/pages/Profile/Styles.js
@@ -1,7 +1,36 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
+  width: 100%;
 
+  > header {
+    width: 100%;
+    height: 144px;
+
+    background-color: ${({ theme }) => theme.COLORS.BACKGROUND_900};
+
+    display: flex;
+    align-items: center;
+    padding: 0 124px;
+
+    svg {
+      font-size: 24px;
+      color: ${({ theme }) => theme.COLORS.GRAY_100};
+    
+    }
+  }
+
+`;
+
+
+export const Form = styled.form`
+
+  max-width: 340px;
+  margin: 30px auto 0;
+
+
+
+  
 
 
 
