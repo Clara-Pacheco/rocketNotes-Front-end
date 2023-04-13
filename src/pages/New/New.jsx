bc657a7e57d89d794d57e28899314ed6f581607c
Clara <clarapachecodev@gmail.com>
2023-04-13 17:09:02 -0300

    Add 3 new NoteItem components

--- src/pages/New/New.jsx
+++ src/pages/New/New.jsx
@@ -25,9 +25,12 @@ export function New(){
             <NoteItem value="https://www.rocketseat.com.br" />
             <NoteItem isNew placeholder="Novo link" />
           </Section>  
+
           <Section title="Marcadores">
             <div className='tags'>
               <NoteItem value="React" />
+              <NoteItem value="NodeJS" />
+              <NoteItem value="Sql" />
               <NoteItem isNew placeholder="Novo marcador" />
             </div>
           </Section>
