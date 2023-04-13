b6d333299fd7ab85bafe5dfd1362fb92df8561ea
Clara <clarapachecodev@gmail.com>
2023-04-13 08:37:36 -0300

    Create folder with SignUp component page and stylization

---
+++ src/pages/SignUp/SignUp.jsx
@@ -0,0 +1,47 @@
+import { Container, Form, Background } from './Styles'
+
+import { Input } from '../../components/Input/Input'
+import { Button } from '../../components/Button/Button'
+
+import {FiMail, FiLock, FiUser} from'react-icons/fi'
+
+export function SignUp() {
+ return (
+  <Container>
+
+  <Background />
+
+  <Form >
+    <h1>Rocket Notes</h1>
+    <p>Aplicação para salvar e gerenciar seus links úteis.</p>
+    <h2>Crie sua conta</h2>
+
+    <Input
+      icon={FiUser}
+      placeholder="Nome"
+      type="text"
+     /> 
+
+    <Input
+      icon={FiMail}
+      placeholder="Email"
+      type="text"
+     /> 
+
+    <Input
+      icon={FiLock}
+      placeholder="Senha"
+      type="password"
+     /> 
+
+     <Button title="Cadastrar" />
+
+      <a href='#'>
+        Voltar para o login
+      </a>
+  </Form>
+
+  </Container>
+ )
+
+}
