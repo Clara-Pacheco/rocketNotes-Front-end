b488ac34738144f3aaef360ce6c15537f727fb75
Clara <clarapachecodev@gmail.com>
2023-04-13 19:13:14 -0300

    Import AppRoutes components and return it inside of BrowserRouter inside of Routes component

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,2 +1,11 @@
 import { BrowserRouter  } from "react-router-dom";
 
+import { AppRoutes  } from "./app.routes";
+
+export function Routes() {
+  return (
+    <BrowserRouter>
+      <AppRoutes />
+    </BrowserRouter>
+  )
+}
