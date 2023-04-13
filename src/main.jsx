deace04a97b830b73585b8ceb95442439ee73f62
Clara <clarapachecodev@gmail.com>
2023-04-13 19:17:04 -0300

    Inside of main.jsx, render the component Routes containing app routes instead of individual component pages

--- src/main.jsx
+++ src/main.jsx
@@ -4,17 +4,15 @@ import ReactDOM from 'react-dom/client'
 import { ThemeProvider } from 'styled-components'
 import GlobalStyles from './styles/global'
 import theme from './styles/theme'
-import { SignIn } from './pages/SignIn/SignIn'
-import { SignUp } from './pages/SignUp/SignUp'
-import { Profile } from './pages/Profile/Profile'
-import { New } from './pages/New/New'
+
+import { Routes } from './routes'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-       <New />
+       <Routes />
     </ThemeProvider>
   </React.StrictMode>,
 )
