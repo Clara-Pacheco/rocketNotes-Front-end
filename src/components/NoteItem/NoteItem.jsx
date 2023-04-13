b1f8613a9a51a7503adeddaf9108e568625bdaaf
Clara <clarapachecodev@gmail.com>
2023-04-13 15:48:15 -0300

    Build NoteItem Structure passing value, onClick, isNew and ...rest as props

---
+++ src/components/NoteItem/NoteItem.jsx
@@ -0,0 +1,27 @@
+import { Container } from './Styles'
+
+import {FiX, FiPlus} from 'react-icons/fi'
+
+export function NoteItem({ isNew, value, onClick, ...rest }) {
+  return (
+    <Container isNew={isNew}>
+      <input
+        type="text"
+        value={value}
+        readOnly={!isNew}
+        {...rest}
+      />
+
+      <button 
+        onClick={onClick}
+        type="button"
+      >
+
+      {isNew ? <FiPlus /> : <FiX />}
+      
+      </button>
+
+
+    </Container>
+  )
+}
