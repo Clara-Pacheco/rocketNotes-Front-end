25b483678af82944d71940fc48419ee19db8e032
Clara <clarapachecodev@gmail.com>
2023-04-13 09:38:47 -0300

    Import Container and Form css components, icons from react-icon lib and Input and Button components to build Profile page component

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,10 +1,46 @@
-import { Container } from "./Styles";
+import { Container, Form } from "./Styles";
 
+import { FiArrowLeft,FiUser, FiMail, FiLock} from 'react-icons/fi';
+
+import { Input } from '../../components/Input/Input'
+import { Button } from '../../components/Button/Button'
 
 export function Profile(){
   return(
     <Container>
-      
+      <header>
+        <a href="#">
+          <FiArrowLeft />
+        </a>  
+      </header>
+    <Form>
+      <Input
+        placeholder="Nome"
+        type="text"
+        icon={FiUser}
+      />
+
+      <Input
+        placeholder="E-mail"
+        type="text"
+        icon={FiMail}
+      />
+
+      <Input
+        placeholder="Senha Atual"
+        type="password"
+        icon={FiLock}
+      />
+
+      <Input
+        placeholder="Nova Senha"
+        type="password"
+        icon={FiLock}
+      />
+
+      <Button title="Salvar" />
+
+    </Form>
     </Container>
   )
 }
